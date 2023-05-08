1162abcb13c63990fa7a6161c7c5eb2c68535e12
atikrangnekar28@gmail.com <atikrangnekar28@gmail.com>
2023-05-08 11:28:23 +0530

    ef

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -9,10 +9,13 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
-    categories.reduce((acc, category) => {
+  (categories) =>{
+    console.log(categories);
+    return categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
     }, {})
+  }
+    
 );
